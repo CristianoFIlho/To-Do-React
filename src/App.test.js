877467e2a8d41790102d630ebecb231b7bfdb840
Cristiano Filho ✅ <54041918+CristianoFIlho@users.noreply.github.com>
2022-06-27 21:18:20 -0300

    spacing to do ✅

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders things to do', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/things to do/i);
+  expect(element).toBeInTheDocument();
 });
