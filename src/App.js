3c761afc0e3e7b65705851a432c951b01a478ea6
Cristiano Filho ✅ <54041918+CristianoFIlho@users.noreply.github.com>
2022-06-27 21:34:17 -0300

    export component react app ✅

--- src/App.js
+++ src/App.js
@@ -23,24 +23,10 @@ function App() {
 
   return (
     <div className="container">
-      <div className="row">
-        <div className="todolist">
-          <h1>{title.toUpperCase()}</h1>
-          <ul className="list-group list-group-flush">
-            {items.map(item => (
-              <li key={item.id} className="list-group-item">
-                <div className="form-check">
-                  <input className="form-check-input" type="checkbox" value="" id={`todo-item-check-${item.id}`} />
-                  <label className="form-check-label" htmlFor={`todo-item-check-${item.id}`}>
-                    {item.text}
-                  </label>
-                </div>
-              </li>
-            ))}
-          </ul>
+            <div className="row">
+                <TodoList title={title} items={items} />
+            </div>
         </div>
-      </div>
-    </div>
   );
 }
 
