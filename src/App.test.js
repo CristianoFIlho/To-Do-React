0f7a389c0467ffd8c8a6bbb843ebe7cd32e16454
Cristiano Filho ✅ <54041918+CristianoFIlho@users.noreply.github.com>
2022-06-28 19:08:33 -0300

    update final to do list react ✅

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders things to do', () => {
-  render(<App />);
-  const element = screen.getByText(/things to do/i);
-  expect(element).toBeInTheDocument();
-});
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './components/wrappers/App';
 
-
+// Add bootstrap
 import 'bootstrap/dist/css/bootstrap.css';
 
-
-
-import './style/index.css';
+// Add our style
+import './assets/style/index.css';
 
 ReactDOM.render(
     <App/>,
